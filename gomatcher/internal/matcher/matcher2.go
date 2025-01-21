bdfa740969645bf7818fbfcd9157bbaf9a51c1d8
Yashupadhyaya <uyash73@gmail.com>
2025-01-21 11:12:22 +0530

    change repo name

---
+++ gomatcher/internal/matcher/matcher2.go
@@ -0,0 +1,11 @@
+package mock_gomock
+
+import (
+	"github.com/golang/mock/gomock"
+)
+
+func OldMockMatcher(ctrl *gomock.Controller) *MockMatcher {
+	mock := &MockMatcher{ctrl: ctrl}
+	mock.recorder = &MockMatcherMockRecorder{mock}
+	return mock
+}
